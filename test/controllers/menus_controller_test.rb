639de561476a4af62482f4079e4b0b7454199f13
Nicolas Beladen <nicolas@beladen.fr>
2026-06-29 10:15:01 +0200

    Clean up: public access, seed, hide devise links

--- test/controllers/menus_controller_test.rb
+++ test/controllers/menus_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class MenusControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get menus_index_url
+    get get menus_url
     assert_response :success
   end
 end
