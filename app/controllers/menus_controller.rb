e41a3694c00b69c9289f303a07f6a0be1c14f3d9
Yassir Khalid <y@ssir.net>
2026-07-01 14:49:59 +0100

    Display last menu recipes on menus index page

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,10 @@
 class MenusController < ApplicationController
   def index
+    @menu = current_user.menus.last
+    if @menu
+      @recipes = @menu.recipes
+    else
+      @recipes = []
+    end
   end
 end
